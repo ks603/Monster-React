6ff82b5c836e66b3bc3cfb142da13b9246623332
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-28 13:24:33 -0400

    Styled app

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
       monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
           handleChange={e => this.setState({ searchField: e.target.value })}
