13110760b549ac77d0a2b15b6be88160cfef2ac4
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-27 21:15:31 -0400

    Filter through monster names

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -19,9 +20,16 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()))
     return (
       <div className="App">
-        <Cardlist monsters={this.state.monsters} ></Cardlist>
+        <input
+          type='search'
+          placeholder='search monsters'
+          onChange={e => this.setState({ searchField: e.target.value })} />
+        <Cardlist monsters={filteredMonsters} ></Cardlist>
       </div>
     );
   }
