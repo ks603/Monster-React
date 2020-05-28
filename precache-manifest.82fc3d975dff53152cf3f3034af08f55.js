4f2a2e353a9d1e41442ac494a0b8934c764310a4
ks603 <kevin.shaughnessy18@gmail.com>
2020-05-28 13:28:42 -0400

    Updates

---
+++ precache-manifest.82fc3d975dff53152cf3f3034af08f55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1463440749d66970aee91e393f9f49",
+    "url": "/Monster-React/index.html"
+  },
+  {
+    "revision": "3bb7b67d8d4fdd41e82a",
+    "url": "/Monster-React/static/css/main.a63f4ab0.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/Monster-React/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-React/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb7b67d8d4fdd41e82a",
+    "url": "/Monster-React/static/js/main.e0d6b454.chunk.js"
+  },
+  {
+    "revision": "3d72ae476e75fb4c5612",
+    "url": "/Monster-React/static/js/runtime-main.bde289e9.js"
+  }
+]);
